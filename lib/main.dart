c8316d46f57e825c402f0dc721a55ef742266703
miradouro <rafaelmiradouro@gmail.com>
2024-08-02 18:51:29 -0300

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( const MaterialApp(
+    //debugShowCheckedModeBanner: false,
+    title: "Frases do dia",
+    home: Home(),
+  ));
 }
 
+class H extends StatefulWidget {
+  const H({super.key});
+
+  @override
+  State<H> createState() => _HState();
+}
+
+class _HState extends State<H> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("F R A S E S"),
+        backgroundColor: Colors.cyan,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
+        child: Text("conteudo principal"),
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        color: Colors.cyan,
+      ),
+    );
+  }
+}
